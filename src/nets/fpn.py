9ca51b10d584dd2b94bd0458f142fb6cc3722014
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2018-01-17 23:24:38 +0200

    changes the initialization of the input placeholders of class and box labels to float32 instead of int64

--- src/nets/fpn.py
+++ src/nets/fpn.py
@@ -44,18 +44,38 @@ class FPN:
         self.init_network()
         self.init_output()
 
+    def init_helper_variables(self):
+        """
+        Create a global step variable to be a reference to the number of iterations
+        Create cur epoch tensor to totally save the process of the training
+        Save the best iou on validation
+        :return:
+        """
+        with tf.variable_scope('global_epoch'):
+            self.global_epoch_tensor = tf.Variable(0, trainable=False, name='global_epoch')
+            self.global_epoch_input = tf.placeholder('int32', None, name='global_epoch_input')
+            self.global_epoch_assign_op = self.global_epoch_tensor.assign(self.global_epoch_input)
+        with tf.variable_scope('global_step'):
+            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
+            self.global_step_input = tf.placeholder('int32', None, name='global_step_input')
+            self.global_step_assign_op = self.global_step_tensor.assign(self.global_step_input)
+        with tf.variable_scope('best_measure'):
+            self.best_measure_tensor = tf.Variable(0.0, trainable=False, name='best_measure')
+            self.best_measure_input = tf.placeholder('float32', None, name='best_measure_input')
+            self.best_measure_assign_op = self.best_measure_tensor.assign(self.best_measure_input)
+
     def init_input(self):
         with tf.name_scope('input'):
             self.x = tf.placeholder(tf.float32, [self.config.batch_size,
                                                  self.config.img_height,
                                                  self.config.img_width,
                                                  self.config.num_channels])
-            self.y_classes = tf.placeholder(tf.int64, [self.config.batch_size,
+            self.y_classes = tf.placeholder(tf.float32, [self.config.batch_size,
                                                        self.config.img_height,
                                                        self.config.img_width,
                                                        self.config.number_of_anchors,
                                                        self.config.number_of_classes])
-            self.y_boxes = tf.placeholder(tf.int64, [self.config.batch_size,
+            self.y_boxes = tf.placeholder(tf.float32, [self.config.batch_size,
                                                      self.config.img_height,
                                                      self.config.img_width,
                                                      self.config.number_of_anchors, 4])
@@ -114,13 +134,7 @@ class FPN:
         with tf.name_scope('output_classes'):
             self.y_out_classes = self.class_subnet1 + self.class_subnet2
         with tf.name_scope('output_boxes'):
-            self.y_out_boxes =
-
-    def _merge_block(self, name, upstream, downstream):
-        upsampled = tf.image.resize_images(downstream, upstream.get_shape(), method=ResizeMethod.NEAREST_NEIGHBOR, align_corners=False)
-        conv1x1 =  self._conv(name, upstream, num_filters=downstream.shape[-1], kernel_size=(1,1))
-        merged = tf.add(upsampled, conv1x1, name)
-        return merged
+            self.y_out_boxes = self.box_subnet1 + self.box_subnet2
 
     def _class_subnet(self, input):
         with tf.variable_scope('class_subnet'):
@@ -129,13 +143,13 @@ class FPN:
                 conv1 = self._relu('relu1', conv1)
             with tf.variable_scope('conv_2_x'):
                 conv2 = tf.layers.conv2d(conv1, 256, [3, 3], padding='SAME', reuse=tf.AUTO_REUSE, name='conv2')
-                conv2 = self._relu('relu2', conv1)
+                conv2 = self._relu('relu2', conv2)
             with tf.variable_scope('conv_3_x'):
                 conv3 = tf.layers.conv2d(conv2, 256, [3, 3], padding='SAME', reuse=tf.AUTO_REUSE, name='conv3')
-                conv3 = self._relu('relu3', conv1)
+                conv3 = self._relu('relu3', conv3)
             with tf.variable_scope('conv_4_x'):
                 conv4 = tf.layers.conv2d(conv3, 256, [3, 3], padding='SAME', reuse=tf.AUTO_REUSE, name='conv4')
-                conv4 = self._relu('relu4', conv1)
+                conv4 = self._relu('relu4', conv4)
             with tf.variable_scope('conv_5_x'):
                 conv5 = tf.layers.conv2d(conv4, self.y_classes * self.y_boxes, [3, 3], padding='SAME', reuse=tf.AUTO_REUSE, name='conv5')
                 conv5 = self._sigmoid('sigmoid5', conv5)
@@ -143,6 +157,12 @@ class FPN:
 
     def _box_subnet(self, input):
         raise NotImplementedError("box subnet not implemented")
+
+    def _merge_block(self, name, upstream, downstream):
+        upsampled = tf.image.resize_images(downstream, upstream.get_shape(), method=ResizeMethod.NEAREST_NEIGHBOR, align_corners=False)
+        conv1x1 =  self._conv(name, upstream, num_filters=downstream.shape[-1], kernel_size=(1,1))
+        merged = tf.add(upsampled, conv1x1, name)
+        return merged
 
     def _residual_block(self, name, x, filters, pool_first=False, strides=1, three_stage=False):
         print('Building residual unit: %s' % name)
