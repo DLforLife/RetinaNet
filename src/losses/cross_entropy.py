41ac47412d7ac2aa46d3ad537a5f4ed8eb6d60c6
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-01-19 20:13:46 +0200

    added implementation for cross entropy

--- src/losses/cross_entropy.py
+++ src/losses/cross_entropy.py
@@ -1,3 +1,28 @@
 """
 Implementation for 2D cross entropy
 """
+import tensorflow as tf
+class CrossEntropy:
+    def __init__(self, logits, labels):
+        """
+        Initialzing the parameters for 2D Cross entropy
+        Args:
+        - logits(Tensor): class predictions of shape [Batch_size, num_classes]
+        - one_hot_labels (Tensor): ground truth one hot lables of shape [Batch_size, num_classes]
+        Returns:
+        - loss
+        """
+        self.logits = logits
+        self.labels = labels
+
+        self.calulate()
+
+    def calulate(self):
+        """
+        Computing the focal loss
+        Args:
+        Returns:
+        """
+        loss = tf.nn.sparse_softmax_with_cross_entropy_with_logits(logits=self.logits, labels=self.labels)
+        loss = tf.reduce_mean(loss)
+        return loss
