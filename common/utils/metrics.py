1d10ac57cad7a320cc17d07f33266cc6e6ba25ae
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2018-01-23 22:50:46 +0200

    creates an empty metrics class

--- common/utils/metrics.py
+++ common/utils/metrics.py
@@ -1,3 +1,15 @@
 """
 Definitions for the metrics used in our network. Each metric is to be implemented as a class. Example: IOU
 """
+
+class Metrics():
+	def __init__(self):
+		self.fp = None
+		self.fn = None
+		self.tp = None
+
+	def precision(self, (y_out_classes, y_classes), (y_out_boxes, y_boxes)):
+		return self.tp/(self.tp + self.fp)
+
+	def recall(self, (y_out_classes, y_classes), (y_out_boxes, y_boxes)):
+		return self.tp/(self.tp + self.fn)
