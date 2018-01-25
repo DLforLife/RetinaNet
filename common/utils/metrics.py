22aeaed5c8d43a2d05d1c5b7674a11199086c144
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2018-01-25 12:47:43 +0200

    adds basic functionality to the update_metrics function

--- common/utils/metrics.py
+++ common/utils/metrics.py
@@ -3,13 +3,36 @@ Definitions for the metrics used in our network. Each metric is to be implemente
 """
 
 class Metrics():
-	def __init__(self):
+	def __init__(self, labels, iou_threshold, conf_threshold):
 		self.fp = None
 		self.fn = None
 		self.tp = None
+		self.precision = None
+		self.recall = None
+		self.labels = labels
+		self.iou_threshold = iou_threshold
+		self.conf_threshold = conf_threshold
 
-	def precision(self, (y_out_classes, y_classes), (y_out_boxes, y_boxes)):
-		return self.tp/(self.tp + self.fp)
+	def update_metrics(self, y_boxes, pred_boxes):
+		detected_boxes = []
+		matched_predictions = []
+		tp = 0
+		fp = 0
+		fn = 0
+		for label in self.labels:
+			y_boxes_label = y_boxes[label]
+			pred_boxes_label = pred_boxes[label]
+			for y_box in y_boxes_label:
+				for pred_box in pred_boxes_label:
+					if(iou(y_box, pred_box)>self.iou_threshold):
+						tp += 1
+						detected_boxes.append(y_box)
+						matched_predictions.append(pred_box)
+		fp = len(pred_boxes) - len(matched_predictions)
+		fn = len(y_boxes) - len(detected_boxes)
+		self.tp += tp
+		self.fp += fp
+		self.fn += fn
+		self.precision = self.tp/(self.tp + self.fp)
+		self.recall = self.tp/(self.tp + self.fn)
 
-	def recall(self, (y_out_classes, y_classes), (y_out_boxes, y_boxes)):
-		return self.tp/(self.tp + self.fn)
