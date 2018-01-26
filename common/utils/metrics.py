a0f7a14d7d2f8e44fb51206a46c7689598365f1d
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2018-01-26 18:12:19 +0200

    adds a condition to avoid multiple detections of the same groundtruth and multiple detections using the same predicted box

--- common/utils/metrics.py
+++ common/utils/metrics.py
@@ -25,10 +25,13 @@ class Metrics():
 			pred_boxes_label = pred_boxes[label]
 			for y_box in y_boxes_label:
 				for pred_box in pred_boxes_label:
-					if (iou(y_box, pred_box) > self.iou_threshold):
+					if (iou(y_box, pred_box) > self.iou_threshold and
+							not y_box in detected_boxes and
+							not pred_box in matched_predictions):
 						tp += 1
 						detected_boxes.append(y_box)
 						matched_predictions.append(pred_box)
+						
 		fp = len(pred_boxes) - len(matched_predictions)
 		fn = len(y_boxes) - len(detected_boxes)
 		self.tp += tp
