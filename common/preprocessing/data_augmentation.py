efb3df9579f57498e9b21ebe6af5ca76097b98ce
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-01-27 23:30:41 +0200

    added vertical flipping function

--- common/preprocessing/data_augmentation.py
+++ common/preprocessing/data_augmentation.py
@@ -17,6 +17,7 @@ class data_augmentation:
         """
         self.images = images
         self.boxes = boxes
+
     def random_horizontal_flip(self, image, boxes, flip_prob=0.5):
         """
         Performs random horizontal flipping to the image and/or box
@@ -25,6 +26,8 @@ class data_augmentation:
         - boxes: ground truth boxes in image (Nx4)
         - flip_prob (float): the probability of flipping the image
         Returns:
+        - flipped_image
+        - flipped_boxes
         """
         with tf.name_scope('RandomHorizontalFlip', values=[image, boxes]):
             img_w = tf.get_shape(image)[0]
@@ -37,12 +40,14 @@ class data_augmentation:
             # flip box
             flipped_boxes = tf.cond(do_a_flip_random, lambda: self.flip_box_horizontally(boxes,img_w), lambda: boxes, img_w)
             return flipped_image, flipped_boxes
+
      def flip_image_horizontally(self, image):
         """
         flip image
         """
         flipped_image = tf.image.flip_left_right(image)
         return flipped_image
+
     def flip_box_horizontally(self, boxes):
         """
         flip box
@@ -54,18 +59,39 @@ class data_augmentation:
 
     def random_vertical_flip(self, flip_prob=0.5):
         """
-        Performs random horizontal flipping to the image and/or box
+        Performs random vertical flipping to the image and/or box
         Args:
+        - image: image data of shape (WxHxC)
+        - boxes: ground truth boxes in image (Nx4)
         - flip_prob (float): the probability of flipping the image
         Returns:
+        - flipped_image
+        - flipped_boxes
         """
+        with tf.name_scope('RandomVerticalFlip', values=[image, boxes]):
+            img_h = tf.get_shape(image)[1]
+            # random variable defining whether to do flip or not
+            do_a_flip_random = tf.random_uniform([], seed=seed)
+            # flip only if there are bounding boxes in the image
+            do_a_flip_random = tf.logical_and(tf.greater(tf.size(boxes), 0), tf.greater(do_a_flip_random, flip_prob))
+            # flip image
+            flipped_image = tf.cond(do_a_flip_random, lambda: self.flip_image_vertically(image), lambda: image)
+            # flip box
+            flipped_boxes = tf.cond(do_a_flip_random, lambda: self.flip_box_vertically(boxes,img_h), lambda: boxes, img_h)
+            return flipped_image, flipped_boxes
+
      def flip_image_vertically(self,image):
         """
         flip image
         """
         flipped_image = tf.image.flip_up_down(image)
         return flipped_image
-    def flip_box_vertically(self,box):
+
+    def flip_box_vertically(self, boxes, img_h):
         """
         flip box
         """
+        x, y, w, h = tf.split(value=boxes, num_or_size_splits=4, axis=1)
+        flipped_y = tf.subract(img_wh - y)
+        flipped_box = tf.concat([x, flipped_y, w, h], 1)
+        return flipped_box
