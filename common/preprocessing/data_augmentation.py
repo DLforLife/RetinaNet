f70dfae31a303de1caa0673fd57cca50772f520a
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-01-27 17:32:48 +0200

    added data augmentation class for any augmentation methods performed in the paper

---
+++ common/preprocessing/data_augmentation.py
@@ -0,0 +1,21 @@
+"""
+Performs Data augmentation on the input data
+Section 4.1: optimization
+"We use horizontal image flipping as the only form of data augmentation unless otherwise noted."
+"""
+import tensorflow as tf
+import numpy as np
+
+class data_augmentation:
+    def __init__(self, images, boxes):
+        """
+        Initializing data augmentation class
+        """
+    def random_horizontal_flip(self):
+        """
+        Performs random horizontal flipping to the images and/or boxes
+        """
+    def random_vertical_flip(self):
+        """
+        Performs random horizontal flipping to the images and/or boxes
+        """
