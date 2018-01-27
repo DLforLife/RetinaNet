ddc71642da26c85feeeeecc381173730747ab890
Hager Rady <10363680+hagerrady13@users.noreply.github.com>
2018-01-27 17:02:04 +0200

    updated nms to sort boxes based on class scores

--- common/postprocessing/filter_anchors.py
+++ common/postprocessing/filter_anchors.py
@@ -9,7 +9,7 @@ def Intersection_over_union(box_one, box_two):
     Calculates intersection over union between 2 boxes
     Args:
     - box_one (list): (xc,yc,w,h,score)
-    - box_two (list): (xc,yc,w,h,score)
+    - box_two (list): (xc,yc,w,h)
     Returns:
     - iou (float): iou value
     """
@@ -37,34 +37,51 @@ def Intersection_over_union(box_one, box_two):
     iou = intersected_area/float(union_area)
     return iou
 
-def non_max_suppression(box_detections, class_scores, threshold=0.5):
+def decode_highest_scores(detector_confidence=0.05):
+    """
+    To improve speed, we only decode box predictions from at most
+    1k top-scoring predictions per FPN level, after thresholding detector confidence at 0.05
+    """
+def non_max_suppression(box_y, box_pred, class_scores=None, sort=False, iou_threshold=0.5):
     """
     Perfoms non max suppresion to choose the max score detections based on IOU
     Args:
-    - box_detections (numpy array):
+    - box_pred (numpy array):
     - class_scores (numpy array):
     - threshold (float):
     Returns:
     - final_detection_list (numpy array):
     """
     print ("Calculating non-max suppression")
-    all_detections = np.concatenate([box_detections, class_scores], axis=1)
-    all_detections = all_detections[all_detections[:,4].argsort()[::-1]]
+
+    if not 0 <= iou_threshold <= 1.0:
+        raise ValueError('iou_thresh must be between 0 and 1')
+
+    if sort:
+        all_detections = np.concatenate([box_pred, class_scores], axis=1)
+        all_detections = all_detections[all_detections[:,4].argsort()[::-1]]
+    else:
+        all_detections = box_pred
 
     final_detection_list = []
-    for bbox in all_detections:
-        for final_bbox in final_detection_list:
-            iou = Intersection_over_union(final_bbox, bbox)
-            if iou > threshold:
-                break
-        else:
-            final_detection_list.append(bbox)
+    for bbox in box_y:
+        for pred_bbox in all_detections:
+            iou = Intersection_over_union(pred_bbox, bbox)
+            if not 0 <= iou <= 1.0:
+                raise ValueError('iou must be between 0.0 and 1.0')
+                continue
+            if iou > iou_threshold:
+                final_detection_list.append(pred_bbox)
 
     return final_detection_list
 
-if __name__ == "__main__":
-    detections = np.array([(11, 11, 24, 24), (10, 11, 20, 20), (11, 9, 24, 24), (40, 42, 20, 20)])
-    scores = np.array([0.75, 0.8, 0.7, 0.6])
-    scores = np.expand_dims(scores, -1)
-    final_detections = non_max_suppression(detections,scores)
-    print ('Final Detections: ' + ', '.join(map(str, final_detections)))
+"""
+For testing..
+"""
+# if __name__ == "__main__":
+#     box_pred = np.array([(11, 11, 24, 24), (10, 11, 20, 20), (11, 9, 24, 24), (40, 42, 20, 20)])
+#     box_y = np.array([(12, 11, 24, 24), (40, 40, 18, 18)])
+#     # scores = np.array([0.75, 0.8, 0.7, 0.6])
+#     # scores = np.expand_dims(scores, -1)
+#     final_detections = non_max_suppression(box_y, box_pred)
+#     print ('Final Detections: ' + ', '.join(map(str, final_detections)))
